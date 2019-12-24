e9af93bffcf5749ae047f0e34d3a9624b92d91b3
Cheng Ly <chengsieuly@gmail.com>
2019-12-23 20:16:48 -0800

    Handle no leaderboard error

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,7 +5,7 @@ import getUserFromId from '../helpers/getUserFromId';
 import formatGraphQLError from '../helpers/formatGraphQLError';
 
 export default {
-    name: 'leaderboard',
+    name: ['leaderboard', 'lb'],
     description: 'Get the leaderboard',
     execute(message, args, bot, apollo) {
         apollo.query({
@@ -13,7 +13,12 @@ export default {
             fetchPolicy: 'no-cache'
         })
             .then(res => {
+                if (!res.data.getLeaderboard) {
+                    return message.channel.send('There is no one on the leaderboards.');
+                }
+
                 const ranks = res.data.getLeaderboard.ranks;
+
                 const tableRanks = ranks.map((playerId, i) => [getUserFromId(playerId, bot).username, i + 1]);
 
                 const leaderboard = table(
