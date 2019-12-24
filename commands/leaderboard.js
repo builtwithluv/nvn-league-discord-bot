b5c46121f773c79cf5b1d61c9dbbced4617f5942
Cheng Ly <chengsieuly@gmail.com>
2019-12-23 19:43:23 -0800

    Add leaderboard command

---
+++ commands/leaderboard.js
@@ -0,0 +1,35 @@
+import gql from 'graphql-tag';
+import table from 'markdown-table';
+import { getLeaderboard } from '../src/graphql/queries';
+import getUserFromId from '../helpers/getUserFromId';
+import formatGraphQLError from '../helpers/formatGraphQLError';
+
+export default {
+    name: 'leaderboard',
+    description: 'Get the leaderboard',
+    execute(message, args, bot, apollo) {
+        apollo.query({
+            query: gql(getLeaderboard),
+            fetchPolicy: 'no-cache'
+        })
+            .then(res => {
+                const ranks = res.data.getLeaderboard.ranks;
+                const tableRanks = ranks.map((playerId, i) => [getUserFromId(playerId, bot).username, i + 1]);
+
+                const leaderboard = table(
+                    [
+                        ['Player', 'Rank'],
+                        ...tableRanks
+                    ],
+                    {
+                        align: ['l', 'c']
+                    }
+                );
+
+                message.channel.send(`\`\`\`${leaderboard}\`\`\``);
+            })
+            .catch(err => {
+                message.channel.send(formatGraphQLError(err.message));
+            });
+    }
+};
